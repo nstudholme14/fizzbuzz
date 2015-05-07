56ce4422eb520a98412c859826795b62f684f41e
Nora Studholme <nstudholme14@cmc.edu>
2015-05-07 14:20:02 -0700

    final commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,42 @@
+//fizzbuzz refactor!
 
-var counter
-for (counter=1; counter < 100; counter ++) 
-{
-	if (counter%15 == 0)  
-		console.log('fizzbuzz');
-	else if (counter%3 == 0)
-		console.log('fizz'); 
-	else if (counter%5 == 0) 
-		console.log ('buzz'); 
-	else
-		console.log(counter);
+var number= prompt("Please enter a number:", 0);
+parseInt(number, 10); 
+
+console.log('number', number);
+
+var $ul = document.querySelector('ul'); 
+
+fizzBuzz(number); 
+
+function present(result) {
+	console.log(result); 
+
+	var $li=document.createElement('li'); 
+		$li.innerText= result; 
+		$ul.appendChild($li); 
+		}
+
+
+function fizzBuzz(number) {
+	console.log('fizzBuzz', number)
+
+for (var counter=1; counter < number; counter ++) {
+	if ((counter%15) == 0)  {
+		present('FizzBuzz'); }
+
+	else if ((counter%3) == 0) {
+		present('Fizz'); }
+
+	else if ((counter%5) == 0) {
+		present('Buzz');  }
+
+	else {
+		present(counter);
+		
+			}
 }
+}
+
 
 
